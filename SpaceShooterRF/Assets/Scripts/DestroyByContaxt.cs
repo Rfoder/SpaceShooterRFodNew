bdf0e51c02caba5b1890fa82c661ac9a0f6fa6b6
Rfoder <bfcat10@yahoo.com>
2016-01-28 13:52:17 -0500

    Finished Pt. 9
    Added the asteroid and DESTROYED IT!

---
+++ SpaceShooterRF/Assets/Scripts/DestroyByContaxt.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContaxt : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "Boundary") 
+		       {
+			return;
+	        	}
+		//	Debug.Log (other.name);
+		Destroy(other.gameObject);
+		Destroy(gameObject);
+		}
+}
