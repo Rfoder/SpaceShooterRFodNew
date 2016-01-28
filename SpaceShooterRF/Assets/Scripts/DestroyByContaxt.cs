93e12cd7a1aac94b4a4b281dc47114ecae853295
Rfoder <bfcat10@yahoo.com>
2016-01-28 14:08:57 -0500

    Finished Pt. 10
    Added some.... EXPLOSIONS, MUHAHAHA!

--- SpaceShooterRF/Assets/Scripts/DestroyByContaxt.cs
+++ SpaceShooterRF/Assets/Scripts/DestroyByContaxt.cs
@@ -3,12 +3,18 @@ using System.Collections;
 
 public class DestroyByContaxt : MonoBehaviour {
 
+	public GameObject explosion;
+	public GameObject playerExplosion;
+
 	void OnTriggerEnter(Collider other){
-		if (other.tag == "Boundary") 
-		       {
-			return;
-	        	}
-		//	Debug.Log (other.name);
+				if (other.tag == "Boundary") {
+						return;
+				}
+				//	Debug.Log (other.name);
+				Instantiate (explosion, transform.position, transform.rotation);
+		if (other.tag == "Player") {
+				Instantiate (playerExplosion, other.transform.position, other.transform.rotation);
+		}
 		Destroy(other.gameObject);
 		Destroy(gameObject);
 		}
